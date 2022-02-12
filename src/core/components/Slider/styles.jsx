0dcf422c196f80d898ceb72c309b1c6a4653db6f
MurylloEx <muryllopimenta@gmail.com>
2022-02-12 19:22:01 -0300

    :sparkles: feature: Created Slider component.

--- src/core/components/Slider/styles.jsx
+++ src/core/components/Slider/styles.jsx
@@ -1,5 +1,53 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
+
+export const Input = styled.input`
+  &[type=range i] {
+    -webkit-appearance: auto;
+    appearance: auto;
+    cursor: default;
+    color: #9d968e;
+    padding: initial;
+    border: initial;
+    margin: 2px;
+    cursor: pointer;
+  }
+
+  & {
+    -webkit-appearance: none;
+    width: 335px;
+    height: 4px;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+`;
+
+export const Button = styled.button`
+  & {
+    margin-right: 16 px;
+    border: 1px solid #7d77a6;
+    color: #7d77a6;
+    background-color: transparent;
+    outline: none;
+    box-shadow: none;
+    cursor: pointer;
+    width: 2.5rem;
+    height: 2.5rem;
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+    border-radius: 50%;
+    transition: all .1s ease-in;
+    font-size: 1.5rem;
+  }
+  &:hover {
+    border: 1px solid #7d67a6;
+    color: #7d67a6;
+  }
+`;
+
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Button, PageLayout, PasswordBox } from "../../core/components";
+import { Button, PageLayout, PasswordBox, Slider } from "../../core/components";
 
 export const HomePage = () => {
   return (
     <PageLayout>
+      <Slider />
       <Button>
         Copiar
       </Button>
