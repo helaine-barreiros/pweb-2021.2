ffc6e13023367578f9139824dcbffa0c88e3ac70
MurylloEx <muryllopimenta@gmail.com>
2022-02-12 17:33:10 -0300

    :sparkles: feature: Created Footer component.

--- src/core/components/Footer/index.jsx
+++ src/core/components/Footer/index.jsx
@@ -1,7 +1,20 @@
 import React, { Fragment } from 'react';
+import { Container, FooterText, Image, ImageGroup, Link } from './styles';
 
 export const Footer = () => {
   return (
-    <Fragment />
+    <Container>
+      <FooterText>
+        Disciplina de Programação III ©
+      </FooterText>
+      <ImageGroup>
+        <Link title="Universidade de Pernambuco" href="http://upe.br">
+          <Image src="assets/asset_upe_blazon_logo.png" />
+        </Link>
+        <Link title="Universidade de Pernambuco" href="http://upe.br/garanhuns">
+          <Image src="assets/asset_upe_multicampi_logo.png" />
+        </Link>
+      </ImageGroup>
+    </Container>
   )
 }
--- src/core/components/Generator/styles.jsx
+++ src/core/components/Generator/styles.jsx
@@ -3,3 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   
 `;
+
+export const Logo = styled.img`
+  width: 200px;
+  height: 200px;
+`;
